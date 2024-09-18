94403ba048505e32e799e74f94878c03fd890bd3
Dicamarques <dcabrantesm@gmail.com>
2024-09-18 15:24:23 +0100

    Declare the driver

--- src/driver/drv_hlw8112.c
+++ src/driver/drv_hlw8112.c
@@ -11,34 +11,34 @@
 #include "drv_spi.h"
 #include "drv_uart.h"
 
-static unsigned short bl0942_baudRate = 4800;
+static unsigned short hlw8112_baudRate = 4800;
 
-#define BL0942_UART_RECEIVE_BUFFER_SIZE 256
-#define BL0942_UART_ADDR 0 // 0 - 3
-#define BL0942_UART_CMD_READ(addr) (0x58 | addr)
-#define BL0942_UART_CMD_WRITE(addr) (0xA8 | addr)
-#define BL0942_UART_REG_PACKET 0xAA
-#define BL0942_UART_PACKET_HEAD 0x55
-#define BL0942_UART_PACKET_LEN 23
+#define HLW8112_UART_RECEIVE_BUFFER_SIZE 256
+#define HLW8112_UART_ADDR 0 // 0 - 3
+#define HLW8112_UART_CMD_READ(addr) (0x58 | addr)
+#define HLW8112_UART_CMD_WRITE(addr) (0xA8 | addr)
+#define HLW8112_UART_REG_PACKET 0xAA
+#define HLW8112_UART_PACKET_HEAD 0x55
+#define HLW8112_UART_PACKET_LEN 23
 
 // Datasheet says 900 kHz is supported, but it produced ~50% check sum errors  
-#define BL0942_SPI_BAUD_RATE 800000 // 900000
-#define BL0942_SPI_CMD_READ 0x58
-#define BL0942_SPI_CMD_WRITE 0xA8
+#define HLW8112_SPI_BAUD_RATE 800000 // 900000
+#define HLW8112_SPI_CMD_READ 0x58
+#define HLW8112_SPI_CMD_WRITE 0xA8
 
 // Electric parameter register (read only)
-#define BL0942_REG_I_RMS 0x03
-#define BL0942_REG_V_RMS 0x04
-#define BL0942_REG_WATT 0x06
-#define BL0942_REG_CF_CNT 0x7
-#define BL0942_REG_FREQ 0x08
-#define BL0942_REG_USR_WRPROT 0x1D
-#define BL0942_USR_WRPROT_DISABLE 0x55
+#define HLW8112_REG_I_RMS 0x03
+#define HLW8112_REG_V_RMS 0x04
+#define HLW8112_REG_WATT 0x06
+#define HLW8112_REG_CF_CNT 0x7
+#define HLW8112_REG_FREQ 0x08
+#define HLW8112_REG_USR_WRPROT 0x1D
+#define HLW8112_USR_WRPROT_DISABLE 0x55
 
 // User operation register (read and write)
-#define BL0942_REG_MODE 0x19
-#define BL0942_MODE_DEFAULT 0x87
-#define BL0942_MODE_RMS_UPDATE_SEL_800_MS (1 << 3)
+#define HLW8112_REG_MODE 0x19
+#define HLW8112_MODE_DEFAULT 0x87
+#define HLW8112_MODE_RMS_UPDATE_SEL_800_MS (1 << 3)
 
 #define DEFAULT_VOLTAGE_CAL 15188
 #define DEFAULT_CURRENT_CAL 251210
@@ -52,7 +52,7 @@ typedef struct {
     int32_t watt;
     uint32_t cf_cnt;
     uint32_t freq;
-} bl0942_data_t;
+} hlw8112_data_t;
 
 static uint32_t PrevCfCnt = CF_CNT_INVALID;
 
@@ -60,7 +60,7 @@ static int32_t Int24ToInt32(int32_t val) {
     return (val & (1 << 23) ? val | (0xFF << 24) : val);
 }
 
-static void ScaleAndUpdate(bl0942_data_t *data) {
+static void ScaleAndUpdate(hlw8112_data_t *data) {
     float voltage, current, power;
     PwrCal_Scale(data->v_rms, data->i_rms, data->watt, &voltage, &current,
                  &power);
@@ -80,7 +80,7 @@ static void ScaleAndUpdate(bl0942_data_t *data) {
     BL_ProcessUpdate(voltage, current, power, frequency, energyWh);
 }
 
-static int BL0942_UART_TryToGetNextPacket(void) {
+static int HLW8112_UART_TryToGetNextPacket(void) {
 	int cs;
 	int i;
 	int c_garbage_consumed = 0;
@@ -88,12 +88,12 @@ static int BL0942_UART_TryToGetNextPacket(void) {
 
 	cs = UART_GetDataSize();
 
-	if(cs < BL0942_UART_PACKET_LEN) {
+	if(cs < HLW8112_UART_PACKET_LEN) {
 		return 0;
 	}
 	// skip garbage data (should not happen)
 	while(cs > 0) {
-        if (UART_GetByte(0) != BL0942_UART_PACKET_HEAD) {
+        if (UART_GetByte(0) != HLW8112_UART_PACKET_HEAD) {
 			UART_ConsumeBytes(1);
 			c_garbage_consumed++;
 			cs--;
@@ -103,30 +103,30 @@ static int BL0942_UART_TryToGetNextPacket(void) {
 	}
 	if(c_garbage_consumed > 0){
         ADDLOG_WARN(LOG_FEATURE_ENERGYMETER,
-                    "Consumed %i unwanted non-header byte in BL0942 buffer\n",
+                    "Consumed %i unwanted non-header byte in HLW8112 buffer\n",
                     c_garbage_consumed);
 	}
-	if(cs < BL0942_UART_PACKET_LEN) {
+	if(cs < HLW8112_UART_PACKET_LEN) {
 		return 0;
 	}
     if (UART_GetByte(0) != 0x55)
 		return 0;
-    checksum = BL0942_UART_CMD_READ(BL0942_UART_ADDR);
+    checksum = HLW8112_UART_CMD_READ(HLW8112_UART_ADDR);
 
-    for(i = 0; i < BL0942_UART_PACKET_LEN-1; i++) {
+    for(i = 0; i < HLW8112_UART_PACKET_LEN-1; i++) {
         checksum += UART_GetByte(i);
 	}
 	checksum ^= 0xFF;
 
-    if (checksum != UART_GetByte(BL0942_UART_PACKET_LEN - 1)) {
+    if (checksum != UART_GetByte(HLW8112_UART_PACKET_LEN - 1)) {
         ADDLOG_WARN(LOG_FEATURE_ENERGYMETER,
                     "Skipping packet with bad checksum %02X wanted %02X\n",
-                    UART_GetByte(BL0942_UART_PACKET_LEN - 1), checksum);
-        UART_ConsumeBytes(BL0942_UART_PACKET_LEN);
+                    UART_GetByte(HLW8112_UART_PACKET_LEN - 1), checksum);
+        UART_ConsumeBytes(HLW8112_UART_PACKET_LEN);
 		return 1;
 	}
 
-    bl0942_data_t data;
+    hlw8112_data_t data;
     data.i_rms =
         (UART_GetByte(3) << 16) | (UART_GetByte(2) << 8) | UART_GetByte(1);
     data.v_rms =
@@ -138,13 +138,13 @@ static int BL0942_UART_TryToGetNextPacket(void) {
     data.freq = (UART_GetByte(17) << 8) | UART_GetByte(16);
     ScaleAndUpdate(&data);
 
-    UART_ConsumeBytes(BL0942_UART_PACKET_LEN);
-	return BL0942_UART_PACKET_LEN;
+    UART_ConsumeBytes(HLW8112_UART_PACKET_LEN);
+	return HLW8112_UART_PACKET_LEN;
 }
 
 static void UART_WriteReg(uint8_t reg, uint32_t val) {
     uint8_t send[5];
-    send[0] = BL0942_UART_CMD_WRITE(BL0942_UART_ADDR);
+    send[0] = HLW8112_UART_CMD_WRITE(HLW8112_UART_ADDR);
     send[1] = reg;
     send[2] = (val & 0xFF);
     send[3] = ((val >> 8) & 0xFF);
@@ -162,7 +162,7 @@ static void UART_WriteReg(uint8_t reg, uint32_t val) {
 static int SPI_ReadReg(uint8_t reg, uint32_t *val) {
 	uint8_t send[2];
 	uint8_t recv[4];
-	send[0] = BL0942_SPI_CMD_READ;
+	send[0] = HLW8112_SPI_CMD_READ;
 	send[1] = reg;
 	SPI_Transmit(send, sizeof(send), recv, sizeof(recv));
 
@@ -181,7 +181,7 @@ static int SPI_ReadReg(uint8_t reg, uint32_t *val) {
 
 static int SPI_WriteReg(uint8_t reg, uint32_t val) {
     uint8_t send[6];
-    send[0] = BL0942_SPI_CMD_WRITE;
+    send[0] = HLW8112_SPI_CMD_WRITE;
     send[1] = reg;
     send[2] = ((val >> 16) & 0xFF);
     send[3] = ((val >> 8) & 0xFF);
@@ -197,7 +197,7 @@ static int SPI_WriteReg(uint8_t reg, uint32_t val) {
     SPI_ReadReg(reg, &read);
     if (read == val ||
         // REG_USR_WRPROT is read back as 0x1
-        (reg == BL0942_REG_USR_WRPROT && val == BL0942_USR_WRPROT_DISABLE &&
+        (reg == HLW8112_REG_USR_WRPROT && val == HLW8112_USR_WRPROT_DISABLE &&
          read == 0x1)) {
         return 0;
     }
@@ -208,7 +208,7 @@ static int SPI_WriteReg(uint8_t reg, uint32_t val) {
     return -1;
 }
 
-static void BL0942_Init(void) {
+static void HLW8112_Init(void) {
     PrevCfCnt = CF_CNT_INVALID;
 
     BL_Shared_Init();
@@ -217,32 +217,32 @@ static void BL0942_Init(void) {
                 DEFAULT_POWER_CAL);
 }
 
-// THIS IS called by 'startDriver BL0942' command
-// You can set alternate baud with 'startDriver BL0942 9600' syntax
-void BL0942_UART_Init(void) {
-	BL0942_Init();
+// THIS IS called by 'startDriver HLW8112' command
+// You can set alternate baud with 'startDriver HLW8112 9600' syntax
+void HLW8112_UART_Init(void) {
+	HLW8112_Init();
 
-	bl0942_baudRate = Tokenizer_GetArgIntegerDefault(1, 4800);
+	hlw8112_baudRate = Tokenizer_GetArgIntegerDefault(1, 4800);
 
-	UART_InitUART(bl0942_baudRate, 0);
-	UART_InitReceiveRingBuffer(BL0942_UART_RECEIVE_BUFFER_SIZE);
+	UART_InitUART(hlw8112_baudRate, 0);
+	UART_InitReceiveRingBuffer(HLW8112_UART_RECEIVE_BUFFER_SIZE);
 
-    UART_WriteReg(BL0942_REG_USR_WRPROT, BL0942_USR_WRPROT_DISABLE);
-    UART_WriteReg(BL0942_REG_MODE,
-                  BL0942_MODE_DEFAULT | BL0942_MODE_RMS_UPDATE_SEL_800_MS);
+    UART_WriteReg(HLW8112_REG_USR_WRPROT, HLW8112_USR_WRPROT_DISABLE);
+    UART_WriteReg(HLW8112_REG_MODE,
+                  HLW8112_MODE_DEFAULT | HLW8112_MODE_RMS_UPDATE_SEL_800_MS);
 }
 
-void BL0942_UART_RunEverySecond(void) {
-    BL0942_UART_TryToGetNextPacket();
+void HLW8112_UART_RunEverySecond(void) {
+    HLW8112_UART_TryToGetNextPacket();
 
-    UART_InitUART(bl0942_baudRate, 0);
+    UART_InitUART(hlw8112_baudRate, 0);
 
-    UART_SendByte(BL0942_UART_CMD_READ(BL0942_UART_ADDR));
-    UART_SendByte(BL0942_UART_REG_PACKET);
+    UART_SendByte(HLW8112_UART_CMD_READ(HLW8112_UART_ADDR));
+    UART_SendByte(HLW8112_UART_REG_PACKET);
 }
 
-void BL0942_SPI_Init(void) {
-	BL0942_Init();
+void HLW8112_SPI_Init(void) {
+	HLW8112_Init();
 
 	SPI_DriverInit();
 	spi_config_t cfg;
@@ -251,22 +251,22 @@ void BL0942_SPI_Init(void) {
 	cfg.polarity = SPI_POLARITY_LOW;
 	cfg.phase = SPI_PHASE_2ND_EDGE;
 	cfg.wire_mode = SPI_3WIRE_MODE;
-	cfg.baud_rate = BL0942_SPI_BAUD_RATE;
+	cfg.baud_rate = HLW8112_SPI_BAUD_RATE;
 	cfg.bit_order = SPI_MSB_FIRST;
 	SPI_Init(&cfg);
 
-    SPI_WriteReg(BL0942_REG_USR_WRPROT, BL0942_USR_WRPROT_DISABLE);
-    SPI_WriteReg(BL0942_REG_MODE,
-                 BL0942_MODE_DEFAULT | BL0942_MODE_RMS_UPDATE_SEL_800_MS);
+    SPI_WriteReg(HLW8112_REG_USR_WRPROT, HLW8112_USR_WRPROT_DISABLE);
+    SPI_WriteReg(HLW8112_REG_MODE,
+                 HLW8112_MODE_DEFAULT | HLW8112_MODE_RMS_UPDATE_SEL_800_MS);
 }
 
-void BL0942_SPI_RunEverySecond(void) {
-    bl0942_data_t data;
-    SPI_ReadReg(BL0942_REG_I_RMS, &data.i_rms);
-    SPI_ReadReg(BL0942_REG_V_RMS, &data.v_rms);
-    SPI_ReadReg(BL0942_REG_WATT, (uint32_t *)&data.watt);
+void HLW8112_SPI_RunEverySecond(void) {
+    hlw8112_data_t data;
+    SPI_ReadReg(HLW8112_REG_I_RMS, &data.i_rms);
+    SPI_ReadReg(HLW8112_REG_V_RMS, &data.v_rms);
+    SPI_ReadReg(HLW8112_REG_WATT, (uint32_t *)&data.watt);
     data.watt = Int24ToInt32(data.watt);
-    SPI_ReadReg(BL0942_REG_CF_CNT, &data.cf_cnt);
-    SPI_ReadReg(BL0942_REG_FREQ, &data.freq);
+    SPI_ReadReg(HLW8112_REG_CF_CNT, &data.cf_cnt);
+    SPI_ReadReg(HLW8112_REG_FREQ, &data.freq);
     ScaleAndUpdate(&data);
 }
